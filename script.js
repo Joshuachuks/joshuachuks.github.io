37fa2cdab2e585807a2a4056aaa9132860ef8ec8
Joshuachuks <163620331+Joshuachuks@users.noreply.github.com>
2024-11-20 23:07:14 -0800

    Update script.js

--- script.js
+++ script.js
@@ -6,14 +6,3 @@ document.addEventListener("DOMContentLoaded", function() {
         navLinks.classList.toggle('showing');
     });
 });
-window.addEventListener('resize', function() {
-    const images = document.qgetElementById('mobile-img');
-    images.forEach(img => {
-        img.style.width = '100%';
-        img.style.height = 'auto';
-    });
-});
-window.addEventListener('resize', function() {
-    const content = document.getElementById('content');
-    content.style.width = window.innerWidth + 'px';
-});
